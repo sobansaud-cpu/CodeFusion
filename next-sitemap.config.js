2ed8b04b5edfb9e6d3671a1eb5db911918d19020
https://github.com/Sobansaud <220893352+sobansaud-cpu@users.noreply.github.com>
2025-10-12 02:35:21 +0500

    main

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://codefusion.site',
+  generateRobotsTxt: true,
+  // optional:
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000
+}
