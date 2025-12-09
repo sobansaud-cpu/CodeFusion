3ecfb92246acd18eb5cfd2295be0d37a86aed280
https://github.com/sobansaud-cpu <ssaud8849@gmail.com>
2025-12-09 12:27:11 +0500

    Version chnage

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,9 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   experimental:{
-//     esmExternals : "loose"
-//   },
-// };
-
-// export default nextConfig;
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   experimental: {
     typedRoutes: true,   // Enable typed routes
-    esmExternals: "loose", // Allow loose esm externals
   },
   typescript: {
     ignoreBuildErrors: true,  // Don’t ignore TS errors during build
